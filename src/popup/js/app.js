69e9fc34e6d479a811fb16017f406029eaee29e0
ailtonloures <ailton.loures99@gmail.com>
2021-07-28 18:11:50 -0300

    feat: styling layout and adding vue configuration

--- src/popup/js/app.js
+++ src/popup/js/app.js
@@ -3,18 +3,48 @@
 const { i18n, storage, identity } = chrome;
 
 // * Components
-const NomeComponent = Vue.extend({
-    data: function() {
-        return;
+const CardComponent = Vue.extend({
+    props: [
+        'site'
+    ],
+    methods: {
+        handleUpdate: function () {
+            console.log("update")
+        },
+        handleDelete: function () {
+            console.log("delete")
+        }
     },
-    template: ''
+    template: `
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">{{ site.title }}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{{ site.url }}</h6>
+            </div>
+            <div class="card-footer text-muted">
+                <span>
+                    <a href="#!" class="i18n card-link link-success" data-i18n="updateButton" @click="handleUpdate">Update</a>
+                    <a href="#!" class="i18n card-link link-danger" data-i18n="deleteButton" @click="handleDelete">Remove</a>
+                </span>
+                <span>2 days ago</span>
+            </div>
+        </div>
+    `
 });
 
 // * Registering components
-Vue.component('nome-component', NomeComponent);
+Vue.component('card-component', CardComponent);
+// * Vue config
+Vue.config.devtools = true;
 
 new Vue({
     el: '#app',
+    data: {
+        site: {
+            title: 'Bootstrap',
+            url: 'https://getbootstrap.com/docs/5.0/components/card/'
+        }
+    },
     methods: {
         i18nActivate: function () {
             const elements = document.getElementsByClassName('i18n');
@@ -25,9 +55,9 @@ new Vue({
 
                 element.innerText = i18n.getMessage(i18nDataset);
             }
-        }
+        },
     },
-    created: function() {
+    mounted: function () {
         this.i18nActivate();
     }
 });
