0eae375b4d13e27ed57aec2fd37c5e3d516ecb90
ailtonloures <ailton.loures99@gmail.com>
2021-07-28 14:48:12 -0300

    :tada: first commit

---
+++ src/popup/js/app.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const { i18n, storage, identity } = chrome;
+
+new Vue({
+    el: '#app',
+    methods: {
+        i18nActivate: function () {
+            const elements = document.getElementsByClassName('i18n');
+
+            for (let i = 0; i < elements.length; i++) {
+                const element = elements.item(i);
+                const i18nDataset = element.dataset.i18n;
+
+                element.innerText = i18n.getMessage(i18nDataset);
+            }
+        }
+    },
+    created: function() {
+        this.i18nActivate();
+    }
+});
+
+const configUser = () => {
+    // todo: check if no exists password and data
+    // * config password here...
+
+    identity.getProfileUserInfo(
+        ({ email, id }) => {
+            if (email !== "" && id !== "") {
+                // * store password and data...
+            }
+            else {
+                // * store only password...
+            }
+        }
+    );
+}
+
