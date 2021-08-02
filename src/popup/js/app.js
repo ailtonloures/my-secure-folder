6f34c42c98b9437834d987414950941119f1126d
ailtonloures <ailton.loures99@gmail.com>
2021-08-01 23:28:33 -0300

    fix: fixing encryptation bug

--- src/popup/js/app.js
+++ src/popup/js/app.js
@@ -1,47 +1,47 @@
-"use strict";
+'use strict';
 
 // * Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDmVZWF30XDbzbjUclI0fOdvplHodmUYcw",
-    authDomain: "my--folder-cc13f.firebaseapp.com",
-    projectId: "my--folder-cc13f",
-    storageBucket: "my--folder-cc13f.appspot.com",
-    messagingSenderId: "982631035022",
-    appId: "1:982631035022:web:361b225a825eeab7ef99df",
+	apiKey: 'AIzaSyDmVZWF30XDbzbjUclI0fOdvplHodmUYcw',
+	authDomain: 'my--folder-cc13f.firebaseapp.com',
+	projectId: 'my--folder-cc13f',
+	storageBucket: 'my--folder-cc13f.appspot.com',
+	messagingSenderId: '982631035022',
+	appId: '1:982631035022:web:361b225a825eeab7ef99df',
 };
 // ? Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 const firestore = firebase.firestore();
-const bookmarks = firestore.collection("bookmarks");
+const bookmarks = firestore.collection('bookmarks');
 
 const { i18n, identity, tabs, storage } = chrome;
 
 // ? Components
-const CardComponent = Vue.extend({
-    props: ["bookmark"],
-    methods: {
-        deleteBookmark: function (id) {
-            bookmarks
-                .doc(id)
-                .delete()
-                .then(() => {
-                    document.getElementById(id).remove();
-                });
-        },
-        goTo: function (url) {
-            window.open(url, "_blank");
-        },
-        formatDate: function (createdAt) {
-            return dayjs(createdAt).format(i18n.getMessage("dateFormat"));
-        },
-    },
-    template: `
+const CardComponent = {
+	props: ['bookmark'],
+	methods: {
+		deleteBookmark: function (id) {
+			bookmarks
+				.doc(id)
+				.delete()
+				.then(() => {
+					document.getElementById(id).remove();
+				});
+		},
+		goTo: function (url) {
+			window.open(url, '_blank');
+		},
+		formatDate: function (createdAt) {
+			return dayjs(createdAt).format(i18n.getMessage('dateFormat'));
+		},
+	},
+	template: `
         <div class="card" v-bind:id="bookmark.uid">
             <div class="card-body" @click="goTo(bookmark.url)" v-bind:title="'Navegar para ' + bookmark.url">
                 <div class="card-title">     
-                    <img v-bind:src="bookmark.favIconUrl" width="18"> 
-                    &nbsp;              
+                    <img v-if="bookmark.favIconUrl" v-bind:src="bookmark.favIconUrl" width="18"> 
+                                 
                     <span>{{ bookmark.title }}</span>
                 </div>
                 <h6 class="card-subtitle mb-2 text-muted">{{ bookmark.url }}</h6>
@@ -57,233 +57,240 @@ const CardComponent = Vue.extend({
             </div>
         </div>
     `,
-});
+};
 
-// ? Registering components
-Vue.component("card-component", CardComponent);
 // ? Vue config
 Vue.config.devtools = true;
 // ? Vue Instance
 const root = new Vue({
-    el: "#root",
-    data: {
-        // * Password form properties
-        passwordVerified: false,
-        passwordExists: false,
-        passwordMessage: null,
-        checkPasswordErrorMessage: null,
-        newPasswordErrorMessage: null,
-        confirmPasswordErrorMessage: null,
-        password: null,
-        newPassword: null,
-        confirmPassword: null,
-        // * User properties
-        user: {
-            id: null,
-            password: null,
-        },
-        // * Bookmarks properties
-        bookmarks: [],
-        bookmarksOnQueue: false,
-    },
-    methods: {
-        i18nActivate: function () {
-            const elements = document.getElementsByClassName("i18n");
+	el: '#root',
+	components: {
+		CardComponent,
+	},
+	data: {
+		// * Password form properties
+		passwordVerified: false,
+		passwordExists: false,
+		passwordMessage: null,
+		checkPasswordErrorMessage: null,
+		newPasswordErrorMessage: null,
+		confirmPasswordErrorMessage: null,
+		password: null,
+		newPassword: null,
+		confirmPassword: null,
+		// * User properties
+		user: {
+			id: null,
+			password: null,
+		},
+		// * Bookmarks properties
+		bookmarks: [],
+		bookmarksOnQueue: false,
+	},
+	methods: {
+		i18nActivate: function () {
+			const elements = document.getElementsByClassName('i18n');
 
-            for (let i = 0; i < elements.length; i++) {
-                const element = elements.item(i);
-                const i18nDataset = element.dataset.i18n;
+			for (let i = 0; i < elements.length; i++) {
+				const element = elements.item(i);
+				const i18nDataset = element.dataset.i18n;
 
-                element.innerHTML = i18n.getMessage(i18nDataset);
-            }
-        },
-        getCurrentUser: function () {
-            identity.getProfileUserInfo(({ id }) => {
-                if (id !== "") {
-                    root.user.id = id;
+				element.innerHTML = i18n.getMessage(i18nDataset);
+			}
+		},
+		getCurrentUser: function () {
+			identity.getProfileUserInfo(({ id }) => {
+				if (id !== '') {
+					root.user.id = id;
 
-                    storage.sync.get(id, function (result) {
-                        if (result.hasOwnProperty(id)) {
-                            root.user.password = result[id];
-                            root.passwordExists = true;
-                            root.passwordMessage = i18n.getMessage("passwordMessage2");
-                        }
-                    });
+					storage.sync.get(id, function (result) {
+						if (result.hasOwnProperty(id)) {
+							root.user.password = result[id];
+							root.passwordExists = true;
+							root.passwordMessage = i18n.getMessage('passwordMessage2');
+						}
+					});
 
-                    localStorage.setItem("userId", id);
-                }
-            });
-        },
-        encryptBookmark: function (bookmark, userId) {
-            const encryptedBookmark = {};
+					localStorage.setItem('userId', id);
+				}
+			});
+		},
+		encryptBookmark: function (bookmark, userId) {
+			const encryptedBookmark = {};
 
-            Object.keys(bookmark).forEach((key) => {
-                encryptedBookmark[key] = CryptoJS.AES.encrypt(bookmark[key].toString(), userId).toString();
-            });
+			Object.keys(bookmark).forEach((key) => {
+				const value = bookmark[key];
 
-            return encryptedBookmark;
-        },
-        decryptBookmark: function (bookmark, userId) {
-            const decryptedBookmark = {};
+				if (value !== null && value !== '' && value !== undefined)
+					encryptedBookmark[key] = CryptoJS.AES.encrypt(value.toString(), userId).toString();
+			});
 
-            Object.keys(bookmark).forEach((key) => {
-                decryptedBookmark[key] = CryptoJS.AES.decrypt(bookmark[key].toString(), userId).toString(CryptoJS.enc.Utf8);
-            });
+			return encryptedBookmark;
+		},
+		decryptBookmark: function (bookmark, userId) {
+			const decryptedBookmark = {};
 
-            return decryptedBookmark;
-        },
-        addBookmark: function () {
-            tabs.query(
-                {
-                    active: true,
-                    currentWindow: true,
-                },
-                (tabs) => {
-                    const { id, favIconUrl, title, url } = tabs[0];
+			Object.keys(bookmark).forEach((key) => {
+				const value = bookmark[key];
 
-                    const date = `${this.now.getFullYear()}-${this.now.getMonth() + 1}-${this.now.getDate()}`;
-                    const hour = `${this.now.getHours()}:${this.now.getMinutes()}`;
-                    const createdAt = `${date} ${hour}`;
+				if (value !== null && value !== '' && value !== undefined)
+					decryptedBookmark[key] = CryptoJS.AES.decrypt(value.toString(), userId).toString(CryptoJS.enc.Utf8);
+			});
 
-                    if (!this.passwordVerified) {
-                        this.bookmarksOnQueue = true;
-                        this.checkPasswordErrorMessage = i18n.getMessage("passwordErrorMessage4");
+			return decryptedBookmark;
+		},
+		addBookmark: function () {
+			tabs.query(
+				{
+					active: true,
+					currentWindow: true,
+				},
+				(tabs) => {
+					const { id, favIconUrl, title, url } = tabs[0];
 
-                        return;
-                    }
+					const date = `${this.now.getFullYear()}-${this.now.getMonth() + 1}-${this.now.getDate()}`;
+					const hour = `${this.now.getHours()}:${this.now.getMinutes()}`;
+					const createdAt = `${date} ${hour}`;
 
-                    const encryptedBookmark = this.encryptBookmark(
-                        {
-                            id,
-                            title,
-                            favIconUrl,
-                            url,
-                        },
-                        this.user.id
-                    );
+					if (!this.passwordVerified) {
+						this.bookmarksOnQueue = true;
+						this.checkPasswordErrorMessage = i18n.getMessage('passwordErrorMessage4');
 
-                    bookmarks
-                        .add({
-                            ...encryptedBookmark,
-                            createdAt,
-                            user: this.user.id,
-                        })
-                        .then((snapshot) => {
-                            this.bookmarks.push({
-                                uid: snapshot.id,
-                                id,
-                                title,
-                                favIconUrl,
-                                url,
-                                createdAt,
-                            });
-                        });
-                }
-            );
-        },
-        getBookmarks: function () {
-            const userId = localStorage.getItem("userId");
+						return;
+					}
 
-            bookmarks
-                .where("user", "==", userId)
-                .get()
-                .then((snapshot) => {
-                    snapshot.docs.forEach((bookmark) => {
-                        const { id, title, favIconUrl, url, createdAt } = bookmark.data();
+					const encryptedBookmark = this.encryptBookmark(
+						{
+							id,
+							title,
+							favIconUrl,
+							url,
+						},
+						this.user.id
+					);
 
-                        const decryptedBookmark = this.decryptBookmark(
-                            {
-                                id,
-                                title,
-                                favIconUrl,
-                                url,
-                            },
-                            userId
-                        );
+					bookmarks
+						.add({
+							...encryptedBookmark,
+							createdAt,
+							user: this.user.id,
+						})
+						.then((snapshot) => {
+							this.bookmarks.push({
+								uid: snapshot.id,
+								id,
+								title,
+								favIconUrl,
+								url,
+								createdAt,
+							});
+						});
+				}
+			);
+		},
+		getBookmarks: function () {
+			const userId = localStorage.getItem('userId');
 
-                        this.bookmarks.push({
-                            uid: bookmark.id,
-                            createdAt,
-                            ...decryptedBookmark,
-                        });
-                    });
-                });
-        },
-        createPassword: function () {
-            const newPassword = this.newPassword;
-            const confirmPassword = this.confirmPassword;
+			bookmarks
+				.where('user', '==', userId)
+				.get()
+				.then((snapshot) => {
+					snapshot.docs.forEach((bookmark) => {
+						const { id, title, favIconUrl, url, createdAt } = bookmark.data();
 
-            this.newPasswordErrorMessage = null;
-            this.confirmPasswordErrorMessage = null;
+						const decryptedBookmark = this.decryptBookmark(
+							{
+								id,
+								title,
+								favIconUrl,
+								url,
+							},
+							userId
+						);
 
-            if (newPassword === null) {
-                this.newPasswordErrorMessage = i18n.getMessage("passwordErrorMessage1");
-                return;
-            }
+						this.bookmarks.push({
+							uid: bookmark.id,
+							createdAt,
+							...decryptedBookmark,
+						});
+					});
+				});
+		},
+		createPassword: function () {
+			const newPassword = this.newPassword;
+			const confirmPassword = this.confirmPassword;
 
-            if (confirmPassword === null || confirmPassword !== newPassword) {
-                this.confirmPasswordErrorMessage = i18n.getMessage("passwordErrorMessage2");
-                return;
-            }
+			this.newPasswordErrorMessage = null;
+			this.confirmPasswordErrorMessage = null;
 
-            storage.sync.set(
-                {
-                    [this.user.id]: newPassword,
-                },
-                () => {
-                    this.user.password = newPassword;
+			if (newPassword === null) {
+				this.newPasswordErrorMessage = i18n.getMessage('passwordErrorMessage1');
+				return;
+			}
 
-                    this.newPassword = null;
-                    this.confirmPassword = null;
-                    this.passwordExists = true;
-                    this.passwordMessage = i18n.getMessage("passwordMessage1");
-                }
-            );
-        },
-        resetPassword: function () {
-            storage.sync.remove(this.user.id, () => {
-                this.passwordExists = false;
-                this.passwordVerified = false;
-                this.passwordMessage = i18n.getMessage("passwordMessage3");
+			if (confirmPassword === null || confirmPassword !== newPassword) {
+				this.confirmPasswordErrorMessage = i18n.getMessage('passwordErrorMessage2');
+				return;
+			}
 
-                this.user.password = null;
-            });
-        },
-        checkPassword: function () {
-            const password = this.password;
+			storage.sync.set(
+				{
+					[this.user.id]: newPassword,
+				},
+				() => {
+					this.user.password = newPassword;
 
-            if (password === null) {
-                this.checkPasswordErrorMessage = i18n.getMessage("passwordErrorMessage1");
-                return;
-            }
+					this.newPassword = null;
+					this.confirmPassword = null;
+					this.passwordExists = true;
+					this.passwordMessage = i18n.getMessage('passwordMessage1');
+				}
+			);
+		},
+		resetPassword: function () {
+			storage.sync.remove(this.user.id, () => {
+				this.passwordExists = false;
+				this.passwordVerified = false;
+				this.passwordMessage = i18n.getMessage('passwordMessage3');
 
-            if (password !== this.user.password) {
-                this.checkPasswordErrorMessage = i18n.getMessage("passwordErrorMessage3");
-                return;
-            }
+				this.user.password = null;
+			});
+		},
+		checkPassword: function () {
+			const password = this.password;
 
-            if (this.bookmarksOnQueue) {
-                this.addBookmark();
-            }
+			if (password === null) {
+				this.checkPasswordErrorMessage = i18n.getMessage('passwordErrorMessage1');
+				return;
+			}
 
-            this.password = null;
-            this.passwordMessage = i18n.getMessage("passwordMessage2");
-            this.passwordVerified = true;
-        },
-    },
-    computed: {
-        now: function () {
-            return new Date();
-        },
-    },
-    created: function () {
-        this.getCurrentUser();
-    },
-    mounted: function () {
-        this.i18nActivate();
-        this.getBookmarks();
-    },
-    updated: function () {
-        this.i18nActivate();
-    },
+			if (password !== this.user.password) {
+				this.checkPasswordErrorMessage = i18n.getMessage('passwordErrorMessage3');
+				return;
+			}
+
+			if (this.bookmarksOnQueue) {
+				this.addBookmark();
+			}
+
+			this.password = null;
+			this.passwordMessage = i18n.getMessage('passwordMessage2');
+			this.passwordVerified = true;
+		},
+	},
+	computed: {
+		now: function () {
+			return new Date();
+		},
+	},
+	created: function () {
+		this.getCurrentUser();
+	},
+	mounted: function () {
+		this.i18nActivate();
+		this.getBookmarks();
+	},
+	updated: function () {
+		this.i18nActivate();
+	},
 });
